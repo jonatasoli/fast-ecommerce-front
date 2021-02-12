4be38448bf14e98c785c0ec47b309bd0547be18a
Thais Martins <thaismartins1999@gmail.com>
2021-02-12 14:58:19 -0300

    fix popup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <v-app>
     <v-content>
+       <cookie-law theme="dark-lime">
+  <div slot-scope="props">
+    <p>
+      Este site utiliza Cookies 🍪
+    </p>
+    <button class="skew" @click="props.accept"><span>Aceitar</span></button>
+    <button class="skew" @click="props.close"><span>Ignorar</span></button>
+  </div>
+ 
+</cookie-law>
       <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
+import CookieLaw from 'vue-cookie-law'
 export default {
   name: "App",
+  components: { CookieLaw },
 
   data: () => ({
     //
   }),
 };
 </script>
+
+<style scoped>
+p, .skew {
+  font-size: 20px;
+}
+.skew {
+  margin-right: 20px;
+  font-weight: bold;
+}
+
+</style>
+
+
--- src/components/shared/SecondaryNavBar.vue
+++ src/components/shared/SecondaryNavBar.vue
@@ -7,30 +7,90 @@
       class="center-items"
     >
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn x-large color="#233237" elevation="0">
-          <router-link 
-          tag="v-btn"
-          :to="{name: 'product-category', 
-          params:{id: category.id}}">{{category.name}}</router-link>
-        </v-btn>
+        <v-btn @click="goHome" x-large color="#233237">INICIO</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn 
+            color="#233237" x-large
+            v-bind="attrs"
+            v-on="on"> 
+              <v-icon
+              small
+              left>mdi-chevron-down</v-icon>
+              <span >Produtos</span>
+            </v-btn>
+          </template>
+             <v-list-item v-for="a in categorys" :key="a.id" link class="item">
+               <router-link
+                tag="v-list-item-title"
+                :to="{name:'ProductCategory', params:{id: a.id}}">
+              <v-list-item-title>{{a.name}}</v-list-item-title>
+             </router-link>
+            </v-list-item>
+          </v-menu>
+        <v-btn @click="goAbout" color="#233237">QUEM SOMOS</v-btn>
+        <v-btn @click="goContact" x-large color="#233237">CONTATO</v-btn>
       </v-toolbar-items>
     </v-toolbar>
   </div>
 </template>
 
 <script>
+import { createNamespacedHelpers } from "vuex";
+const { mapState, mapGetters, mapActions } = createNamespacedHelpers("category");
 export default {
   name: "SecondNavBar",
-  props: ["category"]
+  data(){
+    return {
+      category: this.setCategorys()
+    }
+  },
+  computed: {
+    ...mapState({
+      categorys: "categorys",
+    }),
+  },
+  methods: {
+    ...mapGetters(["getCategory"]),
+    ...mapActions(["setCategorys"]),
+     goHome() {
+      this.$router.push({
+        name: 'Home'
+      })
+    },
+    goAbout() {
+      this.$router.push({
+        name: 'About'
+      })
+    },
+    goContact() {
+      this.$router.push({
+        name: 'Contact'
+      })
+  },
+  beforeRouteUpdate(to, from, next){
+    this.categorys = this.setCategorys()
+    next()
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.categorys = vm.setCategorys();
+      })
+    }
+  }
 }
 </script>
 
-<style lang="sass" scoped>
-.center-items
-  display: flex
-  justify-content: center
+<style scoped>
+.center-items {
+  display: flex;
+  justify-content: center;
   align-items: center
+}
+.theme--dark.v-list-item:not(.v-list-item--active):not(.v-list-item--disabled) {
+    color: #ffffff !important;
+    background-color: #233237;
+    text-transform: uppercase;
+}
 
-v-text-field
-  backgroud: "white"
 </style>
