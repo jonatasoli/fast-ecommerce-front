2a40ff846c3ce3819ca36fbe13e09a704db6046b
Thais Martins <thaismartins1999@gmail.com>
2021-02-19 17:56:42 -0300

    fix create product

---
+++ src/resources/admin/store/mutations_types.js
@@ -0,0 +1,3 @@
+export const GET_PRODUCTS = "GET_PRODUCTS";
+export const SET_ERROR = "SET_ERROR";
+export const SET_ITEM_CREATE = "SET_ITEM_CREATE"
---
+++ src/resources/admin/views/AdminProduct.vue
@@ -0,0 +1,275 @@
+<template>
+  <div>
+    <v-row class="navbar">
+    <NavBar> 
+       <v-btn  x-large 
+        @click.stop="sidebarMenu = !sidebarMenu">
+        <v-icon left> mdi-menu</v-icon></v-btn>
+    </NavBar>
+    <MenuDashboard :sidebarMenu="sidebarMenu"/>
+    </v-row>
+    <v-container class="table">
+        <v-card>
+               <v-data-table 
+                :headers="headers" 
+                :items="items">
+                <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Produtos</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Adicionar
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Nome"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.uri"
+                      label="URI"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.price"
+                      label="Preço"
+                    ></v-text-field>
+                  </v-col>
+                 
+                  <v-col
+                    cols="2"
+                    sm="2"
+                    md="6"
+                  >
+                    <v-textarea
+                      rows="4"
+                      v-model="editedItem.description"
+                      label="Descrição"
+                    ></v-textarea>
+                  </v-col>
+                   <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.quantity"
+                      label="Quantidade"
+                    ></v-text-field>
+
+                    <v-text-field
+                      v-model="editedItem.discount"
+                      label="Desconto"
+                    ></v-text-field>
+                  </v-col>
+                  
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+                
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template >
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+                </v-data-table>
+        </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from "vuex";
+const { mapState, mapGetters, mapActions } = createNamespacedHelpers("productAdmin");
+import NavBar from "@/components/shared/NavBar.vue";
+import MenuDashboard from "../components/MenuDashboard.vue"
+export default {
+  components: {
+    NavBar,
+    MenuDashboard
+  },
+  data(){
+    return {
+      sidebarMenu: false,
+      headers: [
+            { text: 'Id', value: 'id'},
+            { text: 'Nome', value: 'Name' },
+            { text: 'Quantidade', value: 'quantity', sortable: false},
+            { text: 'Actions', value: 'actions', sortable: false },
+          
+      ],
+      items: [],
+      editedIndex: -1,
+      dialog: false, // used to toggle the dialog
+      editedItem: {
+        name: '',
+        uri: '',
+        price: 0,
+        description: '',
+        image_path: 'null',
+        category_id: 1,
+        quantity: 0
+      }, // empty holder for create/update ops
+    }
+  },
+  computed: {
+    ...mapState({
+      productsAll: "products_all"
+    }),
+  },
+  created() {
+    this.getProductsAll()
+  },
+  beforeUpdate() {
+    this.loadItems()
+    console.log(this.items)
+  },
+  methods: {
+    ...mapGetters(["getProductsAll"]),
+    ...mapActions(["setProductsAll", "postProduct"]),
+    close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+    },
+    save() {
+      if (this.editedIndex > -1) {
+          Object.assign(this.items[this.editedIndex], this.editedItem)
+        } else {
+          this.items.push(this.editedItem)
+        }
+        this.postProduct(this.editedItem)
+        this.close()
+    },
+    loadItems() {
+    this.item = []
+    this.items = this.productsAll.map((item) => {
+      return {
+        "id": item.id,
+        "Name": item.name,
+        "quantity": item.quantity
+      }
+    })
+  }
+  },
+   beforeRouteUpdate(to, from, next){
+    this.productsAll = this.setProductsAll();
+    this.items = this.loadItems()
+    next()
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.productsAll = vm.setProductsAll();
+      vm.items = vm.loadItems();
+      })
+    }
+};
+</script>
+
+<style>
+.v-data-table > .v-data-table__wrapper > table > tbody > tr >  td{
+    font-size: 20px !important;
+}
+.v-data-table > .v-data-table__wrapper > table > thead> tr >  th{
+    font-size: 18px !important;
+}
+
+.table {
+    margin-top: 50px;
+}
+</style>
