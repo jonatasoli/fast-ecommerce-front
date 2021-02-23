3438154dc4609e3773dd032d67c640768ed84062
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-02-23 09:22:41 -0300

    create menu "parceiro"

---
+++ src/resources/associate/components/MenuAssociate.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home-style">
+    <v-navigation-drawer
+      v-model="sidebar"
+      app
+      floating
+      :permanent="sidebar"
+      :mini-variant.sync="mini"
+    >
+      <v-list dense dark>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3 class="font-weight-thin">Configurações</h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title
+              ><h3>{{ item.title }}</h3></v-list-item-title
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["sidebarMenu"],
+  computed: {
+    mini() {
+      return this.$vuetify.breakpoint.smAndDown || this.toggleMini;
+    },
+    sidebar: {
+      get() {
+        return this.sidebarMenu;
+      },
+      set(sidebar) {
+        this.$emit("update:sidebarMenu", sidebar);
+      },
+    },
+  },
+  data: () => ({
+    items: [
+      { title: "Painel", to: "/parceiro", icon: "mdi-monitor" },
+      { title: "Produtos", to: "/parceiro/products", icon: "mdi-cart" },
+    ],
+  }),
+};
+</script>
+
+<style></style>
---
+++ src/resources/associate/view/Associate.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <nav-bar>
+      <v-btn x-large @click.stop="sidebarMenu = !sidebarMenu">
+        <v-icon left> mdi-menu</v-icon></v-btn
+      >
+    </nav-bar>
+    <MenuAssociate :sidebarMenu="sidebarMenu"></MenuAssociate>
+    <v-container>
+      <v-row no-gutters>
+        <v-col md="4">
+          <v-card>
+            <h2 class="ml-3">Painel</h2>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import NavBar from "../../../components/shared/NavBar.vue";
+import MenuAssociate from "../components/MenuAssociate";
+export default {
+  components: { NavBar, MenuAssociate },
+  data() {
+    return {
+      sidebarMenu: false,
+    };
+  },
+};
+</script>
+
+<style></style>
