633f4666be9291fe6733abd40fc0d309c68eaea2
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-02-23 09:28:19 -0300

    create associate vuex state

---
+++ src/resources/associate/store/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import {
+  GET_AVAILABLE_AMOUNT,
+  GET_TODAYS_PROFIT,
+  GET_PROFIT,
+  GET_PRODUCTS,
+  SET_ERROR,
+} from "./mutations_types";
+import productAdminService from "@resources/admin/services";
+
+Vue.use(Vuex);
+
+const AssociateModule = {
+  namespaced: true,
+  state: {
+    error: [],
+    available_amount: {
+      total: null,
+      refunt: null,
+    },
+    todays_profit: {
+      daily_gain: null,
+    },
+    profits: [
+      {
+        daily_gain: null,
+        refund: null,
+        day: null,
+      },
+      {
+        daily_gain: null,
+        refund: null,
+        day: null,
+      },
+      {
+        daily_gain: null,
+        refund: null,
+        day: null,
+      },
+    ],
+  },
+};
+
+export default AssociateModule;
