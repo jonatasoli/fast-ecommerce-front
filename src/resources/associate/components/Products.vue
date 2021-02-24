373a4e65720eec24f73da5e09adc7d868cd032e1
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-02-24 12:14:02 -0300

    add Products component

---
+++ src/resources/associate/components/Products.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        class="d-flex align-center justify-space-around flex-wrap"
+        xs="12"
+        sm="12"
+        md="12"
+        lg="12"
+        xl="12"
+        v-if="products.length > 0"
+      >
+      </v-col>
+      <v-col v-else>Não há produtos nessa sessão</v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import ProductCard from "@/resources/product/components/ProductCard.vue";
+
+export default {
+  components: { ProductCard },
+  computed: {
+    ...mapGetters("associate", ["products"]),
+  },
+  methods: {
+    ...mapActions("associate", ["PopulateProducts"]),
+  },
+  created() {
+    this.PopulateProducts();
+  },
+};
+</script>
+
+<style></style>
