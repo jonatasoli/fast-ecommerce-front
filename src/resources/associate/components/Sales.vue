0c508d74c85aa21804ee809be25951d89ecbc4cf
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-02-24 12:45:43 -0300

    add Sales component

---
+++ src/resources/associate/components/Sales.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters("associate", ["transactions"]),
+  },
+};
+</script>
+
+<style></style>
