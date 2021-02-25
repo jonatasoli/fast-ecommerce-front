842012b1a6c5167b0086454fd7618a6cb778b9a6
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-02-25 07:42:13 -0300

    add Reports

--- src/resources/associate/components/Report.vue
+++ src/resources/associate/components/Report.vue
@@ -1,5 +1,32 @@
 <template>
-  <v-container> </v-container>
+  <v-container>
+    <v-row class="flex-wrap">
+      <v-col>
+        <v-card>
+          <v-card-title>Relatório 1</v-card-title>
+          <v-card-text>descrição</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Relatório 2</v-card-title>
+          <v-card-text>descrição</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Relatório 3</v-card-title>
+          <v-card-text>descrição</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-card-title>Relatório 4</v-card-title>
+          <v-card-text>descrição</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
