38fac1234625b23acc9cb481990456ee511e4927
Mateus da Silva Sousa <mateus.wtv@gmail.com>
2021-03-14 13:57:59 -0300

    add store test mutations

--- src/resources/partner/store/mutations_types.js
+++ src/resources/partner/store/mutations_types.js
@@ -1,5 +1,5 @@
 export const GET_PROFIT = "GET_PROFIT";
 export const GET_TODAYS_PROFIT = "GET_TODAYS_PROFIT";
-export const GET_AVAILABLE_AMOUNT = "GET_TOTAL";
+export const GET_AVAILABLE_AMOUNT = "GET_AVAILABLE_AMOUNT";
 export const GET_PRODUCTS = "GET_PRODUCTS";
 export const SET_ERROR = "SET_ERROR";
--- src/resources/partner/tests/unit/store.spec.js
+++ src/resources/partner/tests/unit/store.spec.js
@@ -1,7 +1,6 @@
 import { createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import storeConfig from "../../store";
-import { GET_AVAILABLE_AMOUNT } from "../../store/mutations_types";
 
 describe("Vuex Store", () => {
   const localVue = createLocalVue();
@@ -18,16 +17,36 @@ describe("Vuex Store", () => {
   });
 
   it("should update available_amount", () => {
-    store.commit("partner/GET_TOTAL", {
+    store.commit("partner/GET_AVAILABLE_AMOUNT", {
       available_amount: {
         total: 100,
         refund: 50,
       },
     });
-    // console.log(store.state.partner.available_amount);
     expect(store.state.partner.available_amount).toStrictEqual({
       total: 100,
       refund: 50,
     });
+  });
+  it("should update todays profits", () => {
+    const todays_profit = {
+      daily_gain: 100,
+    };
+    store.commit("partner/GET_TODAYS_PROFIT", { todays_profit });
+    expect(store.state.partner.todays_profit).toStrictEqual(todays_profit);
+  });
+  it("should update profits", () => {
+    const profits = {
+      daily_gain: 100,
+      refund: 10,
+      day: 1,
+    };
+    store.commit("partner/GET_PROFIT", { profits });
+    expect(store.state.partner.profits).toStrictEqual(profits);
+  });
+  it("should update partnerProducts", () => {
+    const products = [];
+    store.commit("partner/GET_PRODUCTS", { products });
+    expect(store.state.partner.partnerProducts).toStrictEqual(products);
   });
 });
