1d13fca749f181f4b3eaecf472ff9da7914642a4
Thais Martins <thaismartins1999@gmail.com>
2021-04-09 10:58:46 -0300

    add responsive shopping cart

--- src/resources/shopping-cart/views/ShoppingCart.vue
+++ src/resources/shopping-cart/views/ShoppingCart.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <NavBar />
-    <v-container v-if="shopping_cart.length > 0">
+    <v-container v-if="shopping_cart.length > 0" class="container">
       <v-row
         class="d-flex flex-row justify-center align-center xs-12 sm-10 md-8"
       >
@@ -13,6 +13,7 @@
                 >Cep incorreto, verifique se usou apenas números e no máximo 8
                 números!</v-alert
               >
+              <div class="calc_shipping">
               <v-text-field
                 label="Insira o Frete"
                 :rules="rules"
@@ -24,20 +25,70 @@
                 Calcular Frete
                 <v-icon>fa-truck</v-icon>
               </v-btn>
+
+              </div>
             </v-card-text>
           </v-card>
           <v-card color="blue-grey darken-4">
             <v-card-title>Carrinho de Compras</v-card-title>
             <v-card-subtitle></v-card-subtitle>
             <v-card-text>
-              <v-simple-table>
+              <div v-for="item in shopping_cart" :key="item.product_id" >
+                <div>
+                <h2 class="product_name">{{item.product_name}}</h2>
+                <div class="items">
+                <div class="quantity">
+                <v-btn
+                  class="mx-2"
+                  fab
+                  dark
+                  small
+                  @click="decrementProduct(item)"
+                >
+                  <v-icon dark>fa-minus-circle</v-icon>
+                </v-btn>
+                <span>{{item.qty}}</span>
+                  <v-btn
+                    class="mx-2"
+                    fab
+                    dark
+                    small
+                    @click="increaseProduct(item)"
+                  >
+                    <v-icon dark>fa-plus-circle</v-icon>
+                  </v-btn>
+                </div>
+                  <h3 class="price_item">{{formatCurrency(getPrice(item)) }}</h3>
+                </div>
+                </div>
+                <hr/>
+              </div>
+              <div class="shipping">
+                <h3>Frete: </h3>
+                  <span v-if="shippingPrice === -1">-</span>
+                  <span v-else-if="shippingPrice === 0">Frete Grátis</span>
+                  <span v-else-if="shippingPrice === -2">Cep Incorreto!</span>
+                  <span v-else>{{ formatCurrency(shippingPrice / 100) }}</span>
+
+              </div>
+              <div class="total_price">
+                <h3>Total: </h3>
+                <span>{{ formatCurrency(getTotalPrice) }}</span>
+              </div>
+              <div class="empty_cart">
+                <v-btn @click="empyCart">
+                    Limpar Carrinho
+                  <v-icon>fa-shopping-cart</v-icon>
+                </v-btn>
+              </div>
+              <!-- <v-simple-table class="table">
                 <template v-slot:default>
                   <thead>
                     <tr>
                       <th scope="col">#</th>
                       <th scope="col">Item:</th>
-                      <th scope="col">Preço Unitário:</th>
                       <th scope="col">Quantidade:</th>
+                      <th></th>
                       <th scope="col">Total:</th>
                     </tr>
                   </thead>
@@ -45,7 +96,6 @@
                     <tr v-for="item in shopping_cart" :key="item.product_id">
                       <th>#</th>
                       <td>{{ item.product_name }}</td>
-                      <td>{{ formatCurrency(item.amount / 100) }}</td>
                       <td>
                         <v-btn
                           class="mx-2"
@@ -67,6 +117,7 @@
                           <v-icon dark>fa-plus-circle</v-icon>
                         </v-btn>
                       </td>
+                      <td></td>
                       <td>{{ formatCurrency(getPrice(item)) }}</td>
                     </tr>
                     <tr>
@@ -88,8 +139,6 @@
                     </tr>
                     <tr>
                       <th scope="row"></th>
-                      <td></td>
-                      <td></td>
                       <td>
                         <v-btn @click="empyCart">
                           Limpar Carrinho
@@ -99,7 +148,7 @@
                     </tr>
                   </tbody>
                 </template>
-              </v-simple-table>
+              </v-simple-table> -->
             </v-card-text>
             <v-card-actions class="d-flex justify-space-between align-center">
               <v-btn
@@ -226,3 +275,65 @@ export default {
   },
 };
 </script>
+<style scoped>
+.calc_shipping{
+  display: flex;
+  align-items: flex-end;
+}
+.calc_shipping v-text-field {
+  width: 10px
+}
+.items {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: white;
+}
+.product_name {
+  color: white;
+  margin-left: 18px;
+  margin-bottom: 10px;
+}
+.quantity {
+  justify-content:center;
+  flex-wrap: wrap;
+}
+
+.quantity span {
+  font-size: 16px;
+}
+
+.price_item {
+  font-size: 25px
+}
+
+.shipping {
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  margin-bottom: 15px
+}
+
+.total_price {
+  display:flex;
+  justify-content:space-between;
+  font-size: 20px
+}
+
+.total_price span, .shipping span {
+  color: white;
+  font-size: 20px
+}
+
+hr {
+  border: 0;
+  height: 2px;
+  background-image: linear-gradient(to right, transparent, #CCC, transparent);  
+  margin: 30px;
+}
+
+.empty_cart {
+  margin-left: -10px;
+  margin-top: 30px;
+}
+</style>
