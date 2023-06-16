4e89f0210fe5b8211d31500ca867ce93692ae562
Thais Martins <thaismartins1999@gmail.com>
2023-06-16 18:43:16 -0300

    feat: migrate project to nuxt3

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="bg-gattoRosa-primary">
+    <div class="flex justify-center items-center p-2">
+      <img
+        width="200"
+        src="~/assets/logo-negativa.png"
+        alt=""
+      >
+    </div>
+    <nav class="bg-gattoRosa-primary py-4">
+      <ul class="flex justify-center gap-12 uppercase">
+        <li>
+          <nuxt-link to="/">
+            Home
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/">
+            Novidades
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/">
+            Celebrity
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- src/mixins/format-currency.js
+++
@@ -1,9 +0,0 @@
-import { currencyFormatter } from "@/utils";
-
-export default {
-  methods: {
-    formatCurrency(value) {
-      return currencyFormatter().format(value);
-    },
-  },
-};
