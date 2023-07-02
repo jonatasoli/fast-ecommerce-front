e3b17c652447b4a149eb15bccb1658ea8cbf31f6
Thais Martins <thaismartins1999@gmail.com>
2023-07-02 19:29:00 -0300

    chore: setup naive ui

---
+++ naive.style.ts
@@ -0,0 +1,102 @@
+import type { GlobalThemeOverrides } from 'naive-ui'
+
+export const theme: GlobalThemeOverrides = {
+  common: {
+    primaryColor: '#DA9A39',
+    primaryColorHover: '#E4A647',
+    primaryColorPressed: '#AC7C34',
+    borderRadius: '10px',
+  },
+  Button: {
+    borderRadiusLarge: '10px',
+    borderRadiusMedium: '10px',
+    borderRadiusSmall: '10px',
+    borderRadiusTiny: '10px',
+    // Default button
+    color: '#FFFFFF',
+    textColor: '#363949',
+    border: '1px solid #363949',
+    colorHover: '#F2F1F4',
+    textColorHover: '#363949',
+    borderHover: '1px solid #363949',
+    colorPressed: '#F2F1F4',
+    textColorPressed: '#363949',
+    borderPressed: '2px solid #363949',
+    colorFocus: '#F2F1F4',
+    textColorFocus: '#363949',
+    borderFocus: '2px solid #363949',
+    rippleColor: '#363949',
+    // Primary button
+    colorFocusPrimary: '#AC7C34',
+    borderFocusPrimary: '1px solid #AC7C34',
+    // Quaternary button
+    colorQuaternaryHover: '#363949',
+    colorQuaternaryPressed: '#363949',
+  },
+  Radio: {
+    buttonBorderColor: '#DA9A39',
+    buttonBorderColorActive: '#AC7C34',
+    buttonBorderColorHover: '#F9E9D2',
+    buttonTextColorHover: '#F9E9D2',
+    buttonTextColor: '#DA9A39',
+    buttonTextColorActive: '#AC7C34',
+    buttonColorActive: '#F9E9D2',
+  },
+  Icon: {
+    color: '#D29841',
+  },
+  Tabs: {
+    barColor: '#D29841',
+    tabTextColorActiveLine: '#D29841',
+    tabTextColorHoverLine: '#D29841',
+  },
+  Tag: {
+    color: '#F9E9D2',
+    textColor: '#D29841',
+    border: '1px solid #D29841',
+    closeIconColor: '#D29841',
+    closeIconColorHover: '#E4A647',
+    borderRadius: '4px',
+  },
+  Select: {
+    peers: {
+      InternalSelection: {
+        boxShadowActive: '#FFFFFF',
+        boxShadowFocus: '#FFFFFF',
+        border: '1px solid #BDB8C7',
+        borderHover: '1px solid #363949',
+        borderActive: '2px solid #363949',
+        borderFocus: '2px solid #363949',
+        borderError: '1px solid #DD3C57',
+      },
+    },
+  },
+  Checkbox: {
+    borderRadius: '2px',
+    colorChecked: '#363949',
+    border: '1px solid #333745',
+    borderFocus: '1px solid #333745',
+    boxShadowFocus: '#FFFFFF',
+    borderChecked: '1px solid #363949',
+  },
+  Collapse: {
+    titleFontSize: '20px',
+    titleFontWeight: 'bold',
+    titleTextColor: '#DA9A39',
+    arrowColor: '#DA9A39',
+  },
+  Dropdown: {
+    optionColorHover: '#F9E9D2',
+    optionTextColorHover: '#D29841',
+    optionColorActive: '#D29841',
+  },
+} as const
+
+export const breakpoints = {
+  xs: 0,
+  s: 840,
+  m: 1024,
+  l: 1280,
+  xl: 1536,
+  xxl: 1920,
+} as const
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,22 @@
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import { LOCALES } from './utils/enums'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
+const {
+  VITEST,
+  NODE_ENV,
+} = process.env
+
+const transpileNaive = NODE_ENV === 'production' || VITEST !== undefined
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   imports: { autoImport: false },
-  modules: ['@nuxtjs/i18n', '@nuxtjs/tailwindcss', '@nuxtjs/device', '@nuxtjs/google-fonts'],
+  modules: [
+    '@nuxtjs/i18n',
+    '@nuxtjs/device',
+    '@nuxtjs/google-fonts',
+  ],
   i18n: {
     baseUrl: process.env.I18N_BASE_URL,
     locales: [{ iso: 'pt-BR', code: LOCALES.PT_BR }],
@@ -13,12 +25,40 @@ export default defineNuxtConfig({
   },
   googleFonts: {
     families: {
-      Poppins: true,
-      Lato: true,
-      Montserrat: true,
+      Montserrat: [400, 500, 600, 700],
       download: true,
       inject: true,
     },
+  },
+  build: {
+    analyze: true,
+    transpile:
+      transpileNaive
+        ? [
+            'naive-ui',
+            '@css-render/vue3-ssr',
+          ]
+        : [],
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        !transpileNaive
+          ? ['naive-ui']
+          : [],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/main.scss";',
+        },
+      },
+    },
+    plugins: [
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+    ],
   },
   runtimeConfig: {
     public: {
