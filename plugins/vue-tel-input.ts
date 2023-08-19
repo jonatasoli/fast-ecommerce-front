cb1c02341b06c96a405d8a3a255063e10ff05805
Thais Martins <thaismartins1999@gmail.com>
2023-08-19 15:44:09 -0300

    chore: update packages

---
+++ plugins/vue-tel-input.ts
@@ -0,0 +1,7 @@
+import VueTelInput from 'vue-tel-input'
+import { defineNuxtPlugin } from '#app'
+import 'vue-tel-input/vue-tel-input.css'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueTelInput)
+})
