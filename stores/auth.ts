862671992331f41fb0973c526d3fcf5a409b2000
Thais Martins <thaismartins1999@gmail.com>
2023-09-07 17:45:53 -0300

    fix: change import types

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,8 +1,8 @@
 import { defineStore } from 'pinia'
-import { LoginResponse } from 'server/api/auth/login.post'
-import { LogoutResponse } from 'server/api/auth/logout.get'
-import { RegisterResponse } from 'server/api/auth/register.post'
 import { useUserStore } from './user'
+import { RegisterResponse } from '@/server/api/auth/register.post'
+import { LogoutResponse } from '@/server/api/auth/logout.get'
+import { LoginResponse } from '@/server/api/auth/login.post'
 import { ref, useFetch } from '#imports'
 
 interface LoginParams {
