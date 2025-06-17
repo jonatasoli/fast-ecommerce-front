8c240ccdd208c1e273a4a1772c558b8be44a2c3a
Jonatas Oliveira <contact@jonatasoliveira.dev>
2025-06-17 19:39:28 +0200

    fix: fix dockerfile build

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-  import { ref } from 'vue' 
   import { useNuxtApp, onBeforeMount } from '#imports'
   import { useUserStore } from '@/stores/user'
   import { useCategoryStore } from '@/stores/categories'
