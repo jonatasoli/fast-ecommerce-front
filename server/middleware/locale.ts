943ef040491ce64b5add4ca10d115a1e174eea1a
jpcamposgda <jpcamposgda@gmail.com>
2025-06-17 13:22:52 -0400

    fix: fix bug in ref locale.ts

--- server/middleware/locale.ts
+++ server/middleware/locale.ts
@@ -2,15 +2,13 @@ import { defineEventHandler, getCookie, getRequestHeader, setCookie } from 'h3'
 
 export default defineEventHandler((event) => {
   const existingLocale = getCookie(event, 'i18n_redirected')
-  const age = ref(60 * 60 * 24 * 30)
-
   const browserLocale =
     getRequestHeader(event, 'accept-language')?.split(',')[0] || 'pt-BR'
 
   if (!existingLocale) {
     setCookie(event, 'i18n_redirected', browserLocale, {
       path: '/',
-      maxAge: age.value,
+      maxAge: 60 * 60 * 24 * 30,
     })
   }
 })
